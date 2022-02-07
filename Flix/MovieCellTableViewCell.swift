38d0846bd691576d207c408de687bafead318283
Angel Moreta <37674133+AngelM512@users.noreply.github.com>
2022-02-06 20:47:53 -0600

    first commit

---
+++ Flix/MovieCellTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCellTableViewCell.swift
+//  Flix
+//
+//  Created by Angel Moreta on 2/6/22.
+//
+
+import UIKit
+
+class MovieCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var synpsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
